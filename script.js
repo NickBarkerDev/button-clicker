6641a88384211ee60ea82af19c030d2db86ca230
NickBarkerDev <nickthedevguy@gmail.com>
2022-05-20 00:32:43 -0700

    attempting to fix referenceError on toggleLogin (only an issue on GH pages, works locally)

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let loggedIn = false;
 function toggleLogin(element) {
     if (!loggedIn){
         element.textContent = "Logout";
+        alert("Login successful. Welcome back!");
         loggedIn = true;
     }
     else {
         element.textContent = "Login";
+        alert("You are now logged out. Come back soon!");
         loggedIn = false;
     }
 }
-
-
